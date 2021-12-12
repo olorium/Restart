ff061f5df15ce644b8c83ad11bb2c3a6f799671c
Alex Vasyliev <olorin2007@gmail.com>
2021-12-12 21:33:02 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Oleksii Vasyliev on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Oleksii Vasyliev on 12.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
